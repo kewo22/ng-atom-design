efc62ea62a1f5801e3047db5cc889b0c9e457ab4
kewo22 <kewinf271@gmail.com>
2022-06-06 23:54:21 +0530

    build err fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LinkComponent } from './components/link/link.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LinkComponent,
     ButtonComponent,
     TextInputComponent
   ],
