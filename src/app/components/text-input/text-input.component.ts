33ecc2628b6c928ab3bf19a0dbae31c9eef94ce6
kewo22 <kewinf271@gmail.com>
2022-06-12 22:27:47 +0530

    style fix

--- src/app/components/text-input/text-input.component.ts
+++ src/app/components/text-input/text-input.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-text-input',
   templateUrl: './text-input.component.html',
-  styleUrls: ['./text-input.component.css']
+  styleUrls: ['./text-input.component.scss']
 })
 export class TextInputComponent implements OnInit {
 
@@ -13,9 +13,8 @@ export class TextInputComponent implements OnInit {
   @Input()
   size: 'sm' | 'md' | 'lg' = 'md';
 
-  @Input()
-  type: 'success' | 'danger' | 'warning' | 'none' = 'success';
-
+  // @Input()
+  // type: 'success' | 'danger' | 'warning' = 'success';
 
   @Input()
   helperText: string | null = null;
@@ -25,13 +24,18 @@ export class TextInputComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  public get labelClasses(): string[] {
+    return ['storybook-input-label'];
+  }
+
   public get wrapperClasses(): string[] {
     return ['storybook-input-wrapper'];
     // return ['storybook-button', `storybook-button--${this.size}`, `storybook-button--${this.type}`];
   }
 
-  public get classes(): string[] {
-    return ['storybook-input', `storybook-input--${this.size}`, `storybook-input--${this.type}`];
+  public get inputClasses(): string[] {
+    return ['storybook-input', `storybook-input--${this.size}`];
+    // `storybook-input--${this.type}`
   }
 
   public get helperTextClasses(): string[] {
